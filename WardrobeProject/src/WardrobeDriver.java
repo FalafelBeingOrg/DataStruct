6564e58278cc182e27c1d97f2a23567eba004129
Falafel <tadgranite@gmail.com>
2023-09-01 19:33:08 -0500

    Set up WardrobeDriver for testing Apparel

--- WardrobeProject/src/WardrobeDriver.java
+++ WardrobeProject/src/WardrobeDriver.java
@@ -1,8 +1,17 @@
+import java.awt.*;
+
 public class WardrobeDriver {
     public static  void
     main(String[] args) {
         Apparel[] wardrobe = new Apparel[10];
 
+        wardrobe[0] = new Apparel();
         wardrobe[1] = new Apparel("Blue", 5.99, Apparel.Condition.NEW);
+
+        for (Apparel item : wardrobe) {
+            if (item != null) {
+                System.out.println(item);
+            }
+        }
     }
 }
