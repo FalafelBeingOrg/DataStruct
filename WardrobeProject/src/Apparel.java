be97e2cdd4203546d5f2149eda7a48b81337c88b
Falafel <tadgranite@gmail.com>
2023-09-01 19:30:14 -0500

    Set up apparel class

--- WardrobeProject/src/Apparel.java
+++ WardrobeProject/src/Apparel.java
@@ -1,2 +1,55 @@
 public class Apparel {
+    public enum Condition{
+        NEW("new"),
+        GOOD("good"),
+        POOR("poor"),
+        TRASHED("trashed");
+        private final String mCond;
+        Condition(String cond){
+            mCond = cond;
+        }
+        public String toString(){ return mCond; }
+    };
+    String color;
+    double price;
+    Condition cond;
+    Apparel(){
+        color = null;
+        price = 0.0;
+        cond = Condition.NEW;
+    }
+    Apparel(String color, double price, Condition cond){
+        this.color = color;
+        this.price = price;
+        this.cond = cond;
+    }
+    public String
+    getColor() {
+        return this.color;
+    }
+    public double
+    getPrice() {
+        return this.price;
+    }
+    public Condition
+    getCond() {
+        return this.cond;
+    }
+
+    public void
+    setColor(String color) {
+        this.color = color;
+    }
+    public void
+    setPrice(double price){
+        this.price = price;
+    }
+
+    public void setCond(Condition cond) {
+        this.cond = cond;
+    }
+    public String
+    toString() {
+        return "Color: " + color + ". Price: $" + price + ". Condition: " + cond + ".";
+    }
 }
--- WardrobeProject/src/WardrobeDriver.java
+++ WardrobeProject/src/WardrobeDriver.java
@@ -1,6 +1,8 @@
 public class WardrobeDriver {
     public static  void
     main(String[] args) {
+        Apparel[] wardrobe = new Apparel[10];
 
+        wardrobe[1] = new Apparel("Blue", 5.99, Apparel.Condition.NEW);
     }
 }
