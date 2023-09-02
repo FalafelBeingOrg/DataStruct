3bb31a07542e75d1bd07f7fbd0e93d18ba6f19f3
Falafel <tadgranite@gmail.com>
2023-09-01 20:01:00 -0500

    Wrote Pants

--- WardrobeProject/src/Pants.java
+++ WardrobeProject/src/Pants.java
@@ -1,2 +1,32 @@
-public class Pants {
+public class Pants extends Apparel{
+    int waist;
+    int inseam;
+
+    Pants(){
+        waist = 0;
+        inseam = 0;
+    }
+    Pants(int waist, int inseam) {
+        this.waist = waist;
+        this.inseam = inseam;
+    }
+
+    public int getWaist() {
+        return waist;
+    }
+    public int getInseam() {
+        return inseam;
+    }
+
+    public void setWaist(int waist) {
+        this.waist = waist;
+    }
+    public void setInseam(int inseam) {
+        this.inseam = inseam;
+    }
+
+    public String
+    toString() {
+        return "This " + color + " pair of pants has a(n) " + waist + " waist and a(n)" + inseam + " inch inseam. It cost $" + price + " and is in " + cond + " condition.";
+    }
 }
