153cf689d7c678affe0cf7ec2a0c8f4bd124a483
Falafel <tadgranite@gmail.com>
2023-09-01 19:47:28 -0500

    Wrote Shirt

--- WardrobeProject/src/Shirt.java
+++ WardrobeProject/src/Shirt.java
@@ -1,2 +1,53 @@
-public class Shirt {
+public class Shirt extends Apparel{
+    public enum Size{
+        SMALL("S"),
+        MED("M"),
+        LARGE("L"),
+        XLARGE("XL");
+        private final String mSize;
+        Size(String size){
+            mSize = size;
+        }
+        public String toString(){ return mSize; }
+    };
+
+    Size size;
+    String text;
+
+    Shirt(){
+        size = Size.MED;
+        text = null;
+    }
+    Shirt(String color, double price, Condition cond, Size size, String text){
+        this.color = color;
+        this.price = price;
+        this.cond = cond;
+        this.size = size;
+        this.text = text;
+    }
+
+    public Size getSize() {
+        return size;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setSize(Size size) {
+        this.size = size;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String
+    toString() {
+        String returnString = "This size " + size + " " + color + " shirt cost $" + price + " and is in " + cond + " condition.";
+        if (text != null) {
+            returnString += " It contains the text \"" + text + ".\"";
+        }
+        return returnString;
+    }
 }
